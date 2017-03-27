aa5811ebdfd76ae73c4b00563cf60695f7a461ce
keziasuhendra <keziasuhendra44@gmail.com>
2017-03-27 21:01:50 +0700

    kesu

--- vz03/cell/Park.java
+++ vz03/cell/Park.java
@@ -1,21 +1,21 @@
 /**
-  * File : Park.java
-  * Kelas Park merepresentasikan taman.
-  * @author Sylvia Juliana - 13515070
-  */
+ * File : Park.java
+ * Kelas Park merepresentasikan taman.
+ * @author Sylvia Juliana - 13515070
+ */
 public class Park extends Cell {
   /**
-    * Render dari Park.
-    * @return Mengembalikan char yang merupakan representasi kode Park.
-    */
-  public char Render() {
+   * Render dari Park.
+   * @return Mengembalikan char yang merupakan representasi kode Park.
+   */
+  public char render() {
   	return '#';
   }
   /**
-    * Melakukan cloning untuk menciptakan objek Park baru.
-    * @return Mengembalikan Park.
-    */
-  public Park Clone() {
+   * Melakukan cloning untuk menciptakan objek Park baru.
+   * @return Mengembalikan Park.
+   */
+  public Park clone() {
     return new Park(this);
   }
 }
--- vz03/cell/Road.java
+++ vz03/cell/Road.java
@@ -1,21 +1,21 @@
 /**
-  * File : Road.java
-  * Kelas Road merepresentasikan jalan.
-  * @author Sylvia Juliana - 13515070
-  */
+ * File : Road.java
+ * Kelas Road merepresentasikan jalan.
+ * @author Sylvia Juliana - 13515070
+ */
 public class Road extends Cell {
   /**
-    * Render dari Road.
-    * @return Mengembalikan char yang merupakan representasi kode Road.
-    */
-  public char Render() {
+   * Render dari Road.
+   * @return Mengembalikan char yang merupakan representasi kode Road.
+   */
+  public char render() {
   	return '+';
   }
   /**
-    * Melakukan cloning untuk menciptakan objek Road baru.
-    * @return Mengembalikan Road.
-    */
-  public Road Clone() {
+   * Melakukan cloning untuk menciptakan objek Road baru.
+   * @return Mengembalikan Road.
+   */
+  public Road clone() {
     return new Road(this);
   }
 }
