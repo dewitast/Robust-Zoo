30bcef752b503cb09f65a56a503e22c8b4fc5477
keziasuhendra <keziasuhendra44@gmail.com>
2017-03-27 22:36:49 +0700

    kesu

--- vz03/animal/Iguana.java
+++ vz03/animal/Iguana.java
@@ -1,73 +1,73 @@
 /** 
-  * File : Iguana.java
-  * Kelas Iguana merupakan kelas untuk real object Iguana.
-  * @author Catherine Almira - 13515111
-  */
+ * File : Iguana.java
+ * Kelas Iguana merupakan kelas untuk real object Iguana.
+ * @author Catherine Almira - 13515111
+ */
 public class Iguana extends Iguana {
   /**
-    * Constructor.
-    * Menciptakan objek Iguana.
-    */
+   * Constructor.
+   * Menciptakan objek Iguana.
+   */
   public Iguana() {
     super(6, 0.3, true);
   }
   /**
-    * Constructor dengan parameter.
-    * Menciptakan objek Iguana dengan berat w, jumlah makanan f, dan status jinak t.
-    * @param w Berat Iguana.
-    * @param f Jumlah makanan Iguana.
-    * @param t Status jinak Iguana.
-    */
+   * Constructor dengan parameter.
+   * Menciptakan objek Iguana dengan berat w, jumlah makanan f, dan status jinak t.
+   * @param w Berat Iguana.
+   * @param f Jumlah makanan Iguana.
+   * @param t Status jinak Iguana.
+   */
   public Iguana(double w, double f, boolean t) {
     super(w, f, t);
   }
   /**
-    * Jenis makanan yang dimakan Iguana.
-    * @return Mengembalikan string yang merepresentasikan jenis makanan Iguana.
-    */
-  public String GetFoodType() {
+   * Jenis makanan yang dimakan Iguana.
+   * @return Mengembalikan string yang merepresentasikan jenis makanan Iguana.
+   */
+  public String getFoodType() {
     return "Herbivore";
   }
   /**
-    * Memeriksa apakah Iguana adalah hewan darat.
-    * @return Mengembalikan true jika Iguana adalah hewan darat dan false jika Iguana bukan hewan darat.
-    */
-  public boolean IsLand() {
+   * Memeriksa apakah Iguana adalah hewan darat.
+   * @return Mengembalikan true jika Iguana adalah hewan darat dan false jika Iguana bukan hewan darat.
+   */
+  public boolean isLandAnimal() {
     return true;
   }
   /**
-    * Memeriksa apakah Iguana adalah hewan air.
-    * @return Mengembalikan true jika Iguana adalah hewan air dan false jika Iguana bukan hewan air.
-    */
-  public boolean IsWater() {
+   * Memeriksa apakah Iguana adalah hewan air.
+   * @return Mengembalikan true jika Iguana adalah hewan air dan false jika Iguana bukan hewan air.
+   */
+  public boolean isWaterAnimal() {
     return false;
   }
   /**
-    * Memeriksa apakah Iguana adalah hewan udara.
-    * @return Mengembalikan true jika Iguana adalah hewan udara dan false jika Iguana bukan hewan udara.
-    */
-  public boolean IsFlying() {
+   * Memeriksa apakah Iguana adalah hewan udara.
+   * @return Mengembalikan true jika Iguana adalah hewan udara dan false jika Iguana bukan hewan udara.
+   */
+  public boolean isFlyingAnimal() {
     return false;
   }
   /**
-    * Interaksi yang dilakukan Iguana.
-    * @return Mengembalikan string yang merepresentasikan suara Iguana.
-    */
-  public String Interact() {
+   * Interaksi yang dilakukan Iguana.
+   * @return Mengembalikan string yang merepresentasikan suara Iguana.
+   */
+  public String interact() {
     return "Purrrr";
   }
   /**
-    * Render dari Iguana.
-    * @return Mengembalikan char yang merupakan representasi kode Iguana.
-    */
-  public char Render() {
+   * Render dari Iguana.
+   * @return Mengembalikan char yang merupakan representasi kode Iguana.
+   */
+  public char render() {
     return 'I';
   }
   /**
-    * Melakukan cloning untuk menciptakan objek Iguana baru.
-    * @return Mengembalikan objek Iguana baru.
-    */
-  public Iguana Clone() {
+   * Melakukan cloning untuk menciptakan objek Iguana baru.
+   * @return Mengembalikan objek Iguana baru.
+   */
+  public Iguana clone() {
     return new Iguana(this);
   }
 }
--- vz03/animal/Lion.java
+++ vz03/animal/Lion.java
@@ -1,73 +1,73 @@
 /**
-  * File : Lion.java
-  * Kelas Lion merupakan kelas untuk real object Lion.
-  * @author Kezia Suhendra - 13515063
-  */
+ * File : Lion.java
+ * Kelas Lion merupakan kelas untuk real object Lion.
+ * @author Kezia Suhendra - 13515063
+ */
 public class Lion extends Mammal {
   /**
-    * Constructor.
-    * Menciptakan objek Lion.
-    */
+   * Constructor.
+   * Menciptakan objek Lion.
+   */
   public Lion() {
     super(150, 7.5, false);
   }
   /**
-    * Constructor dengan parameter.
-    * Menciptakan objek Lion dengan berat w, jumlah makanan f, dan status jinak t.
-    * @param w Berat Lion.
-    * @param k Jumlah makanan Lion.
-    * @param t Status jinak Lion.
-    */
+   * Constructor dengan parameter.
+   * Menciptakan objek Lion dengan berat w, jumlah makanan f, dan status jinak t.
+   * @param w Berat Lion.
+   * @param k Jumlah makanan Lion.
+   * @param t Status jinak Lion.
+   */
   public Lion(double w, double f, boolean t) {
     super(w, f, t);
   }
   /**
-    * Jenis makanan yang dimakan Lion.
-    * @return Mengembalikan string yang merepresentasikan jenis makanan Lion.
-    */
-  public String GetFoodType() {
+   * Jenis makanan yang dimakan Lion.
+   * @return Mengembalikan string yang merepresentasikan jenis makanan Lion.
+   */
+  public String getFoodType() {
     return "Carnivore";
   }
   /**
-    * Memeriksa apakah Lion adalah hewan darat.
-    * @return Mengembalikan true jika Lion adalah hewan darat dan false jika Lion bukan hewan darat.
-    */
-  public boolean IsLand() {
+   * Memeriksa apakah Lion adalah hewan darat.
+   * @return Mengembalikan true jika Lion adalah hewan darat dan false jika Lion bukan hewan darat.
+   */
+  public boolean isLandAnimal() {
     return true;
   }
   /**
-    * Memeriksa apakah Lion adalah hewan air.
-    * @return Mengembalikan true jika Lion adalah hewan air dan false jika Lion bukan hewan air.
-    */
-  public boolean IsWater() {
+   * Memeriksa apakah Lion adalah hewan air.
+   * @return Mengembalikan true jika Lion adalah hewan air dan false jika Lion bukan hewan air.
+   */
+  public boolean isWaterAnimal() {
     return false;
   }
   /**
-    * Memeriksa apakah Lion adalah hewan udara.
-    * @return Mengembalikan true jika Lion adalah hewan udara dan false jika Lion bukan hewan udara.
-    */
-  public boolean IsFlying() {
+   * Memeriksa apakah Lion adalah hewan udara.
+   * @return Mengembalikan true jika Lion adalah hewan udara dan false jika Lion bukan hewan udara.
+   */
+  public boolean isFlyingAnimal() {
     return false;
   }
   /**
-    * Interaksi yang dilakukan Lion.
-    * @return Mengembalikan string yang merepresentasikan suara Lion.
-    */
-  public String Interact() {
+   * Interaksi yang dilakukan Lion.
+   * @return Mengembalikan string yang merepresentasikan suara Lion.
+   */
+  public String interact() {
     return ("Roaarr!");
   }
   /**
-    * Render dari Lion.
-    * @return Mengembalikan char yang merupakan representasi kode Lion.
-    */
-  public char Render() {
+   * Render dari Lion.
+   * @return Mengembalikan char yang merupakan representasi kode Lion.
+   */
+  public char render() {
     return 'L';
   }
   /**
-    * Melakukan cloning untuk menciptakan objek Lion baru.
-    * @return Mengembalikan objek Lion baru.
-    */
-  public Lion Clone() {
+   * Melakukan cloning untuk menciptakan objek Lion baru.
+   * @return Mengembalikan objek Lion baru.
+   */
+  public Lion clone() {
     return new Lion(this);
   }
 }
