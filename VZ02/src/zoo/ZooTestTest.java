6bff1c4fd4ec49a001ecd0ec3f42c3769a514835
dewitast <dewitast20@gmail.com>
2017-03-28 19:29:20 +0700

    style

--- VZ02/src/zoo/ZooTestTest.java
+++ VZ02/src/zoo/ZooTestTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package zoo;
 
 import static org.junit.Assert.*;
@@ -8,58 +5,57 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * Testing zoo.
  * @author nim_13515063
- *
  */
 public class ZooTestTest {
+  /**
+   * Test method for {@link zoo.ZooTest#testZoo()}.
+   */
 
- /**
-  * Test method for {@link zoo.ZooTest#testZoo()}.
-  */
- @Test
- public void testTestZoo() {
-  Zoo z = new Zoo();
-  assertEquals(0, z.getBaris());
-  assertEquals(0, z.getKolom());
-  assertEquals(0, z.getJumlahCage());
- }
+  @Test
+  public void testTestZoo() {
+    Zoo z = new Zoo();
+    assertEquals(0, z.getBaris());
+    assertEquals(0, z.getKolom());
+    assertEquals(0, z.getJumlahCage());
+  }
+  /**
+   * Test method for {@link zoo.ZooTest#testZooIntInt()}.
+   */
 
- /**
-  * Test method for {@link zoo.ZooTest#testZooIntInt()}.
-  */
- @Test
- public void testTestZooIntInt() {
-  Zoo z = new Zoo(2, 4);
-  assertEquals(2, z.getBaris());
-  assertEquals(4, z.getKolom());
-  assertEquals(0, z.getJumlahCage());
- }
+  @Test
+  public void testTestZooIntInt() {
+    Zoo z = new Zoo(2, 4);
+    assertEquals(2, z.getBaris());
+    assertEquals(4, z.getKolom());
+    assertEquals(0, z.getJumlahCage());
+  }
+  /**
+   * Test method for {@link zoo.ZooTest#testGetBaris()}.
+   */
 
- /**
-  * Test method for {@link zoo.ZooTest#testGetBaris()}.
-  */
- @Test
- public void testTestGetBaris() {
-  Zoo z = new Zoo(2, 4);
-  assertEquals(2, z.getBaris());
- }
+  @Test
+  public void testTestGetBaris() {
+    Zoo z = new Zoo(2, 4);
+    assertEquals(2, z.getBaris());
+  }
+  /**
+   * Test method for {@link zoo.ZooTest#testGetKolom()}.
+   */
 
- /**
-  * Test method for {@link zoo.ZooTest#testGetKolom()}.
-  */
- @Test
- public void testTestGetKolom() {
-  Zoo z = new Zoo(2, 4);
-  assertEquals(4, z.getKolom());
- }
+  @Test
+  public void testTestGetKolom() {
+    Zoo z = new Zoo(2, 4);
+    assertEquals(4, z.getKolom());
+  }
+  /**
+   * Test method for {@link zoo.ZooTest#testGetJumlahCage()}.
+   */
 
- /**
-  * Test method for {@link zoo.ZooTest#testGetJumlahCage()}.
-  */
- @Test
- public void testTestGetJumlahCage() {
-  Zoo z = new Zoo();
-  assertEquals(0, z.getJumlahCage());
- }
-
+  @Test
+  public void testTestGetJumlahCage() {
+    Zoo z = new Zoo();
+    assertEquals(0, z.getJumlahCage());
+  }
 }
