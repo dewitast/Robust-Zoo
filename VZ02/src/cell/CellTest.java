37f98d12bb760356e064fdfa81edbe724d4dc00a
calmira <almiratjann@gmail.com>
2017-03-28 23:39:26 +0700

    a

--- VZ02/src/cell/CellTest.java
+++ VZ02/src/cell/CellTest.java
@@ -1,9 +1,13 @@
 
 package cell;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.*;
+import static org.junit.Assert.assertFalse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Testing cell.
