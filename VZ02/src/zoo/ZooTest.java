83250274d086324a2e1b8dca5c8586b74acb9883
sylviajuliana <sjsylviajuliana@gmail.com>
2017-03-28 20:34:13 +0700

    a

--- VZ02/src/zoo/ZooTest.java
+++ VZ02/src/zoo/ZooTest.java
@@ -1,34 +1,65 @@
+/**
+ * 
+ */
 package zoo;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ZooTest {
+/**
+ * @author nim_13515063
+ *
+ */
+public class ZooTestTest {
 
+ /**
+  * Test method for {@link zoo.ZooTest#testZoo()}.
+  */
  @Test
- public void testZoo() {
-  fail("Not yet implemented");
+ public void testTestZoo() {
+  Zoo z = new Zoo();
+  assertEquals(0, z.getBaris());
+  assertEquals(0, z.getKolom());
+  assertEquals(0, z.getJumlahCage());
  }
 
+ /**
+  * Test method for {@link zoo.ZooTest#testZooIntInt()}.
+  */
  @Test
- public void testZooIntInt() {
-  fail("Not yet implemented");
+ public void testTestZooIntInt() {
+  Zoo z = new Zoo(2, 4);
+  assertEquals(2, z.getBaris());
+  assertEquals(4, z.getKolom());
+  assertEquals(0, z.getJumlahCage());
  }
 
+ /**
+  * Test method for {@link zoo.ZooTest#testGetBaris()}.
+  */
  @Test
- public void testGetBaris() {
-  fail("Not yet implemented");
+ public void testTestGetBaris() {
+  Zoo z = new Zoo(2, 4);
+  assertEquals(2, z.getBaris());
  }
 
+ /**
+  * Test method for {@link zoo.ZooTest#testGetKolom()}.
+  */
  @Test
- public void testGetKolom() {
-  fail("Not yet implemented");
+ public void testTestGetKolom() {
+  Zoo z = new Zoo(2, 4);
+  assertEquals(4, z.getKolom());
  }
 
+ /**
+  * Test method for {@link zoo.ZooTest#testGetJumlahCage()}.
+  */
  @Test
- public void testGetJumlahCage() {
-  fail("Not yet implemented");
+ public void testTestGetJumlahCage() {
+  Zoo z = new Zoo();
+  assertEquals(0, z.getJumlahCage());
  }
 
 }
