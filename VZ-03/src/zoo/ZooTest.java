8351d7280a0c64bb86fffa7d3dc5d1e6c9ac652b
keziasuhendra <keziasuhendra44@gmail.com>
2017-03-28 17:34:08 +0700

    kesu

---
+++ VZ-03/src/zoo/ZooTest.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package zoo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author nim_13515063
+ *
+ */
+public class ZooTest {
+
+	/**
+	 * Test method for {@link zoo.Zoo#Zoo()}.
+	 */
+	@Test
+	public void testZoo() {
+		Zoo z = new Zoo();
+		assertEquals(0, z.getBaris());
+		assertEquals(0, z.getKolom());
+		assertEquals(0, z.getJumlahCage());
+	}
+	
+	/**
+	 * Test method for {@link zoo.Zoo#Zoo(int, int)}.
+	 */
+	@Test
+	public void testZooIntInt() {
+		Zoo z = new Zoo(5, 10);
+		assertEquals(5, z.getBaris());
+		assertEquals(10, z.getKolom());
+		assertEquals(0, z.getJumlahCage());
+	}
+
+	/**
+	 * Test method for {@link zoo.Zoo#getBaris()}.
+	 */
+	@Test
+	public void testGetBaris() {
+		Zoo z = new Zoo(5, 10);
+		assertEquals(5, z.getBaris());
+	}
+
+	/**
+	 * Test method for {@link zoo.Zoo#getKolom()}.
+	 */
+	@Test
+	public void testGetKolom() {
+		Zoo z = new Zoo(5, 10);
+		assertEquals(10, z.getKolom());
+	}
+
+	/**
+	 * Test method for {@link zoo.Zoo#getJumlahCage()}.
+	 */
+	@Test
+	public void testGetJumlahCage() {
+		Zoo z = new Zoo();
+		assertEquals(0, z.getJumlahCage());
+	}
+
+}
---
+++ VZ02/src/driver/DriverTest.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package driver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author nim_13515063
+ *
+ */
+public class DriverTest {
+
+	/**
+	 * Test method for {@link driver.Driver#Driver()}.
+	 */
+	@Test
+	public void testDriver() {
+		Driver d = new Driver();
+		assertEquals(-1, d.getPoint().getAbsis());
+		assertEquals(-1, d.getPoint().getOrdinat());
+	}
+
+	/**
+	 * Test method for {@link driver.Driver#getZoo()}.
+	 */
+	@Test
+	public void testGetZoo() {
+		Driver d = new Driver();
+		assertEquals(0, d.getZoo().getJumlahCage());
+		assertEquals(0, d.getZoo().getBaris());
+		assertEquals(0, d.getZoo().getKolom());
+	}
+
+	/**
+	 * Test method for {@link driver.Driver#getPoint()}.
+	 */
+	@Test
+	public void testGetPoint() {
+		Driver d = new Driver();
+		assertEquals(-1, d.getPoint().getAbsis());
+		assertEquals(-1, d.getPoint().getOrdinat());;
+	}
+
+}
